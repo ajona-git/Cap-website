ad508bcdd05be8bfe5498bcbb23bbd8077618fa8
ajona <dodoajona@gmail.com>
2026-01-28 14:22:25 +0100

    generate chekout page using JS. Make adjustments to the styles on the page. added content to the cart for practice

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,4 +1,13 @@
-export const cart=[];
+export const cart=[{
+  productId: '57ccaabdb8e0bd729525c6ed86da63ce',
+  quantity: 2
+},{
+  productId: '3adedaafd92197a9528496e2bb7bb33b',
+  quantity:2
+},{ productId: '401a0a5f6001abec394b8a0d70f226a9',
+  quantity: 2
+}
+];
 export function addToCart(productId){
    let matchingItem;
     cart.forEach((cartItem)=>{
